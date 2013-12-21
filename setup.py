965ec91ab644489438acbfe1421e28749d0c7245
Ivan Kravets <me@ikravets.com>
2013-12-21 23:03:08 +0200

    Registered “smartanthill” package at  https://pypi.python.org/pypi/smartanthill

---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+from smartanthill import __version__
+
+
+setup(
+    name='smartanthill',
+    version=__version__,
+    description='An intelligent micro-oriented networked system',
+    long_description=open("README.rst").read(),
+    author='Ivan Kravets',
+    author_email='me@ikravets.com',
+    url='https://github.com/ivankravets/smartanthill',
+    license='MIT Licence',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Twisted',
+        'Intended Audience :: Customer Service',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Manufacturing',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python',
+        'Topic :: Adaptive Technologies',
+        'Topic :: Communications',
+        'Topic :: Home Automation',
+        'Topic :: Internet',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Software Development :: Compilers',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Networking',
+        'Topic :: Terminals :: Serial'
+    ],
+    install_requires=map(lambda l: l.strip(),
+                         open("requirements.txt").readlines()),
+    packages=find_packages()+['twisted.plugins'],
+    package_data={
+        '': ['*.yaml'],
+        'twisted': ['plugins/smartanthill_plugin.py']
+    },
+    include_package_data=True
+)
