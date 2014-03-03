d6f49e48bec752d4df3fb01832736bad216e0be5
Ivan Kravets <me@ikravets.com>
2014-03-03 21:38:28 +0200

    Python 2.6 fixes, added PyLint conf

--- smartanthill/__init__.py
+++ smartanthill/__init__.py
@@ -1,16 +1,17 @@
 # Copyright (C) Ivan Kravets <me@ikravets.com>
 # See LICENSE for details.
 
+#pylint: disable=W1401
+
 VERSION = (0, 0, 0)
-__version__ = '.'.join(map(str, VERSION))
-__docformat__ = 'restructuredtext'
+__version__ = ".".join(map(str, VERSION))
 __banner__ = """
       _________________________________________
      /                                        /\\
     /        \/             \\\\             __/ /\\
    /   ___  _@@    Smart    @@_  ___     /  \\/
   /   (___)(_)    Anthill    (_)(___)   /__
- /    //|| ||   {:^11}   || ||\\\\     /\\
+ /    //|| ||   {0:^11}   || ||\\\\     /\\
 /_______________________________________/ /
 \_______________________________________\/
  \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \\
