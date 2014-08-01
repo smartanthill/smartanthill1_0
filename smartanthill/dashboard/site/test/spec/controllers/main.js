2aa31139222132cbaa2c47d8d4da4c6e86c76c23
Ivan Kravets <me@ikravets.com>
2014-08-01 21:18:17 +0300

    Preparation Dashboard for testing

--- smartanthill/dashboard/site/test/spec/controllers/main.js
+++ smartanthill/dashboard/site/test/spec/controllers/main.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: SiteController', function () {
 
   // load the controller's module
   beforeEach(module('siteApp'));
 
-  var MainCtrl,
+  var SiteController,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    SiteController = $controller('SiteController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(scope.awesomeThings.length).toBe(3);
+  // });
 });
